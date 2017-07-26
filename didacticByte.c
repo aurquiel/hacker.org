a2c7f067a7956fe03233b5afc0092e05e578b497
void <aurquiel@gmail.com>
2017-07-26 16:45:40 -0400

    commit new chanllenge solution

---
+++ didacticByte.c
@@ -0,0 +1,17 @@
+/*Challenge 'Didactic Byte' [Crypto]
+
+Understanding bits and bytes is key to a hacker's foundation. 
+Let us take the following decimal number:
+233
+This number can just fit in one byte. How would you write it
+ in hexidecimal? (Sometimes people write hex numbers with a
+ '0x' prefix, but here just give the digits, please.) */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+	printf("%x \n",233);
+	return EXIT_SUCCESS;
+}
