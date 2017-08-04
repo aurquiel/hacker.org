ea2a78447db7d5f20c803c7f303b687e75823cfe
aurquiel <aurquiel@gmail.com>
2017-08-03 21:57:01 -0400

    Update didacticXORCipher3.c

--- didacticXORCipher3.c
+++ didacticXORCipher3.c
@@ -1,4 +1,9 @@
-//I CAN'T SOLVED IT
+/*Challenge 'Didactic XOR Cipher 3' [Crypto]
+Here is a string of bytes encoded in hex:
+31cf55aa0c91fb6fcb33f34793fe00c72ebc4c88fd57dc6ba71e71b759d83588
+This sequence has been encrypted with a cipher that works as follows. The first byte has been XOR'd with a byte we'll call 'b'. There is a another component to the key, a byte we'll call 'x'. Each time a byte gets XOR'd by b, the following change is executed:
+b = (b + x) % 256
+In other words, the cipher byte changes with each character encrypted. */
 
 #include <stdio.h>
 #include <string.h>
@@ -6,42 +11,61 @@
 
 int main(int argc, char* argv[])
 {
-  const char* string = "31cf55aa0c91fb6fcb33f34793fe00c72ebc4c88fd57dc6ba71e71b759d83588";
+const char* string = "31cf55aa0c91fb6fcb33f34793fe00c72ebc4c88fd57dc6ba71e71b759d83588";
 
-  char* myByte = (char*)malloc(3); //allocate memory for the bye to read.
+char* myByte = (char*)malloc(3); //allocate memory for the bye to read.
 
-   for(int b=0; b<256; b++)
+for(int b=0; b<256; b++)
+{
+for(int x=0; x<256; x++)
+{
+
+  int bTest = b; //saving the value of b for not chaging inside the other for cicle :)
+  char* myTestResult = (char*)malloc(strlen(string));
+  char flag = 0;
+  
+  for(int i=0; i<strlen(string); i+=2)
   {
-    for(int x=0; x<256; x++)
+
+    strncpy(myByte,string+i,2);
+
+    //Tranform to hex number
+    char *end;
+    unsigned long int number = strtoul(myByte,&end,16);
+
+    if ( (bTest^number)<32 || (bTest^number)>122 )
     {
+      flag = 1;
+      free(myTestResult);
+      break;
+    }
+    else
+    {
+      *(myTestResult+strlen(myTestResult)) = bTest^number;
+      *(myTestResult+strlen(myTestResult)+1) = '\0';
+    }
 
-      int bTest = b; //saving the value of b for not chaging inside the other for cicle :)
-    
-      for(int i=0; i<strlen(string); i+=2)
-      {
-      
-	strncpy(myByte,string+i,2);
+    bTest = (bTest + x) % 256;
 
-	//Tranform to hex number
-	char *end;
-	unsigned long int number = strtoul(myByte,&end,16);
+  } //End of myByte
 
-	if ( ( (bTest^number)<64 && ( (bTest^number)!=32)  ) || (bTest^number)>122 )
-	  break;
+  if (!flag)
+  {
+    printf("%s\n",myTestResult);
+    free(myTestResult);
+  }
+  else
+  {
+    flag = 0;
+  }
 
-	printf  ("%c",(char)(bTest^number));
-	
-	bTest = (bTest + x) % 256;
-	
-      } //End of myByte
-      
-    } //End of x 
+} //End of x 
 
-  } //End of b
+} //End of b
 
-  free(myByte); //Free memory allocation
-    
-  return 0;
+free(myByte); //Free memory allocation
+
+return 0;
 }
 
 
